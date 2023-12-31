cddeb35f1cec0b3f947e82c3400b94706fead613
kirandhabale22 <155295754+kirandhabale22@users.noreply.github.com>
2023-12-31 22:52:25 +0530

    Add files via upload

---
+++ gui_calculater.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def calculate():
+    try:
+        equation = entry.get()
+        result = eval(equation)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, str(result))
+    except Exception as e:
+        messagebox.showerror("Error", "Invalid input or operation")
+
+root = tk.Tk()
+root.title("Calculator")
+root.configure(bg="#1E1E1E")
+
+entry = tk.Entry(root, width=20, font=('Arial', 20, 'bold'), bg="#303030", fg="white", borderwidth=0)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+button_config = {'padx': 15, 'pady': 15, 'width': 2, 'height': 1, 'font': ('Arial', 12)}
+
+buttons = ['7', '8', '9', '/',
+           '4', '5', '6', '*',
+           '1', '2', '3', '-',
+           '0', 'C', '=', '+']
+
+r = 1
+c = 0
+for btn_text in buttons:
+    if btn_text == '=':
+        equals_button = tk.Button(root, text=btn_text, command=calculate, **button_config, bg="#424242", fg="white", borderwidth=0)
+        equals_button.grid(row=r, column=c, padx=5, pady=5)
+    elif btn_text == 'C':
+        clear_button = tk.Button(root, text=btn_text, command=lambda: entry.delete(0, tk.END), **button_config, bg="#424242", fg="white", borderwidth=0)
+        clear_button.grid(row=r, column=c, padx=5, pady=5)
+    else:
+        btn = tk.Button(root, text=btn_text, command=lambda t=btn_text: entry.insert(tk.END, t), **button_config, bg="#424242", fg="white", borderwidth=0)
+        btn.grid(row=r, column=c, padx=5, pady=5)
+    c += 1
+    if c > 3:
+        c = 0
+        r += 1
+
+root.mainloop()
